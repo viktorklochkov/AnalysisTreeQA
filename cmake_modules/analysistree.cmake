d7512a9450280760dd0f37127962c9e994e482ca
Viktor Klochkov <klochkov44@gmail.com>
2020-09-24 16:41:03 +0200

    Update analysistree.cmake

--- cmake_modules/analysistree.cmake
+++ cmake_modules/analysistree.cmake
@@ -3,8 +3,8 @@ set(AnalysisTree_INCLUDE_DIR ${AnalysisTree_INSTALL_DIR}/include)
 set(AnalysisTree_LIBRARY_DIR ${AnalysisTree_INSTALL_DIR}/lib)
 
 ExternalProject_Add(AnalysisTree_Ext
-        GIT_REPOSITORY  "https://github.com/viktorklochkov/AnalysisTree.git"
-        GIT_TAG         "master"
+        GIT_REPOSITORY  "https://github.com/HeavyIonAnalysis/AnalysisTree.git"
+        GIT_TAG         "v1.0.2"
         UPDATE_DISCONNECTED ${UPDATE_DISCONNECTED}
         SOURCE_DIR      "${EXTERNAL_DIR}/AnalysisTree_src"
         BINARY_DIR      "${EXTERNAL_DIR}/AnalysisTree_build"
