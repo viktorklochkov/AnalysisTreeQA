6b1761f0a641d7358f4f89d736079f32a6d3f7cc
Viktor Klochkov <klochkov44@gmail.com>
2020-09-24 16:06:08 +0200

    Update analysistree.cmake
    add AT tag

--- cmake_modules/analysistree.cmake
+++ cmake_modules/analysistree.cmake
@@ -4,7 +4,7 @@ set(AnalysisTree_LIBRARY_DIR ${AnalysisTree_INSTALL_DIR}/lib)
 
 ExternalProject_Add(AnalysisTree_Ext
         GIT_REPOSITORY  "https://github.com/viktorklochkov/AnalysisTree.git"
-        GIT_TAG         "master"
+        GIT_TAG         "v1.0.2"
         UPDATE_DISCONNECTED ${UPDATE_DISCONNECTED}
         SOURCE_DIR      "${EXTERNAL_DIR}/AnalysisTree_src"
         BINARY_DIR      "${EXTERNAL_DIR}/AnalysisTree_build"
